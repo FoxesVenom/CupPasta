f882d04225b1fdf1a2065f87b20b0bf4ac145997
Juwantogo <simmonsjuwan@gmail.com>
2017-02-10 11:12:50 -0500

    2/10/17

---
+++ app/src/main/java/com/mobileapp/design1/cuppasta/HomeScreen.java
@@ -0,0 +1,13 @@
+package com.mobileapp.design1.cuppasta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
