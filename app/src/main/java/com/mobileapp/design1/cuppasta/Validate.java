067e1c5f82f5ba3d313dc61b6fe1ac7748a57b7e
Juwantogo <simmonsjuwan@gmail.com>
2017-04-13 09:20:21 -0400

    Ordering Done Email Says Sent But Doesn't send check PHP

--- app/src/main/java/com/mobileapp/design1/cuppasta/Validate.java
+++ app/src/main/java/com/mobileapp/design1/cuppasta/Validate.java
@@ -14,6 +14,18 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 import java.util.HashMap;
 
@@ -22,14 +34,40 @@ public class Validate extends AppCompatActivity implements View.OnClickListener{
     private Boolean pass2 = false;
     private String Email, Inv, Name, Street, City, State, Zip,Pasta, Sauce, Panini, Dessert, Party, Total;
     private TextView address, summary, invoice, total;
+    DateFormat formatDateTime = DateFormat.getDateTimeInstance();
+    Calendar dateTime = Calendar.getInstance();
+    private TextView text;
+    private Button btn_date;
+    private Button btn_time;
+    private String time;
 
 
     private Button Val;
-    private static final String CONFIRM_URL = "http://173.170.13.161/updateinv.php";
+    private static final String VALID_URL = "http://173.170.13.161/valid.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_validate);
+        dateTime.set(Calendar.SECOND, 00);
+        text = (TextView) findViewById(R.id.txt_TextDateTime);
+        btn_date = (Button) findViewById(R.id.btn_datePicker);
+        btn_time = (Button) findViewById(R.id.btn_timePicker);
+
+        btn_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateDate();
+            }
+        });
+
+        btn_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateTime();
+            }
+        });
+
+        updateTextLabel();
 
         SharedPreferences preferences = getSharedPreferences(Config.SHARED_PREF_NAME, Context.MODE_PRIVATE);
         Email = preferences.getString(Config.EMAIL_SHARED_PREF, "");
@@ -77,16 +115,63 @@ public class Validate extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     public void onClick(View v) {
+        SharedPreferences preferences = getSharedPreferences(Config.SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        Email = preferences.getString(Config.EMAIL_SHARED_PREF, "");
+        Inv = preferences.getString(Config.INVNUM, "");
+        Name = preferences.getString(Config.NAME, "");
+        Street = preferences.getString(Config.STREET, "");
+        City = preferences.getString(Config.CITY, "");
+        State = preferences.getString(Config.STATE, "");
+        Zip = preferences.getString(Config.ZIP, "");
+        Pasta = preferences.getString(Config.PASTA, "");
+        Sauce = preferences.getString(Config.SAUCE, "");
+        Panini = preferences.getString(Config.PANINI, "");
+        Dessert = preferences.getString(Config.DESSERT, "");
+        Party = preferences.getString(Config.SERVE, "");
+        Total = preferences.getString(Config.TOTAL, "");
         if(v == Val){
             Validate();
         }
     }
 
     private void Validate() {
-        CatInfo("2", Inv);
+        time = text.getText().toString();
+        CatInfo("2", Inv, Email, time);
     }
 
-    private void CatInfo(String pass, String inv) {
+    private void updateDate(){
+        new DatePickerDialog(this, d, dateTime.get(Calendar.YEAR),dateTime.get(Calendar.MONTH),dateTime.get(Calendar.DAY_OF_MONTH)).show();
+    }
+
+    private void updateTime(){
+        new TimePickerDialog(this, t, dateTime.get(Calendar.HOUR_OF_DAY), dateTime.get(Calendar.MINUTE), false).show();
+    }
+
+    DatePickerDialog.OnDateSetListener d = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            dateTime.set(Calendar.YEAR, year);
+            dateTime.set(Calendar.MONTH, monthOfYear);
+            dateTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateTextLabel();
+        }
+    };
+
+    TimePickerDialog.OnTimeSetListener t = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            dateTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            dateTime.set(Calendar.MINUTE, minute);
+            dateTime.set(Calendar.SECOND, 00);
+            updateTextLabel();
+        }
+    };
+
+    private void updateTextLabel(){
+        text.setText(formatDateTime.format(dateTime.getTime()));
+    }
+
+    private void CatInfo(String pass, String inv, String email, String time) {
         class subCat extends AsyncTask<String, Void, String> {
             ProgressDialog loading;
             RegisterUserClass ruc = new RegisterUserClass();
@@ -102,9 +187,32 @@ public class Validate extends AppCompatActivity implements View.OnClickListener{
             protected void onPostExecute(String s) {
                 super.onPostExecute(s);
                 loading.dismiss();
-                if (s.trim().equalsIgnoreCase("Catering info updated!")) {
+                if (s.trim().equalsIgnoreCase("Email Sent!")) {
                     Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
-                    Intent ordering = new Intent(Validate.this, Validate.class);
+                    SharedPreferences preferences = getSharedPreferences(Config.SHARED_PREF_NAME,Context.MODE_PRIVATE);
+
+                    //Creating editor to store values to shared preferences
+                    SharedPreferences.Editor editor = preferences.edit();
+
+                    //Adding values to editor
+                    editor.putString(Config.SERVE, "SERVE");
+                    editor.putString(Config.PASTA, "PASTA");
+                    editor.putString(Config.PANINI, "PANINI");
+                    editor.putString(Config.DESSERT, "DESSERT");
+                    editor.putString(Config.SAUCE, "SAUCE");
+                    editor.putString(Config.PHONE, "1234567890");
+                    editor.putString(Config.STREET, "STREET");
+                    editor.putString(Config.ZIP, "12345");
+                    editor.putString(Config.CITY, "CITY");
+                    editor.putString(Config.STATE, "STATE");
+                    editor.putString(Config.INVNUM, "0");
+                    editor.putString(Config.PAYMENT, "PAY");
+
+
+                    //Saving values to editor
+                    editor.commit();
+                    Intent ordering = new Intent(Validate.this, HomeScreen.class);
+                    ordering.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                     startActivity(ordering);
                 }
                 Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
@@ -115,19 +223,16 @@ public class Validate extends AppCompatActivity implements View.OnClickListener{
 
                 HashMap<String, String> data = new HashMap<String,String>();
                 data.put("Pass",params[0]);
-                data.put("Address",params[1]);
-                data.put("City",params[2]);
-                data.put("State",params[3]);
-                data.put("Zip",params[4]);
-                data.put("Phone",params[5]);
-                data.put("inv",params[6]);
+                data.put("Inv",params[1]);
+                data.put("Email",params[2]);
+                data.put("Time",params[3]);
 
-                String result = ruc.sendPostRequest(CONFIRM_URL,data);
+                String result = ruc.sendPostRequest(VALID_URL,data);
                 return  result;
             }
         }
 
         subCat ru = new subCat();
-        ru.execute(pass,inv);
+        ru.execute(pass,inv, email, time);
     }
 }
